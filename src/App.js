52d357cf3ec5ae0d2fb973b571bc8530faff46dd
Alfred024 <21030761tcelaya.edu.mx>
2023-02-12 13:32:13 -0600

    Component MovieItem creado y estilos agregados

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+//Imports de Componentes
+import { MovieItem } from './components/MovieItem';
+
+const moviesDemo = [
+  {name: "Batman", poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmoviepostermexico.com%2Fproducts%2Fposter-pelicula-the-batman-2022&psig=AOvVaw2zG6pBuPTsy5ElRp_A8n7S&ust=1676314145136000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIiutfzSkP0CFQAAAAAdAAAAABAb", year: 2022},
+
+  {name: "Pinochio", poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnecir.org%2Fguillermo-del-toros-pinocchioreview-a-novel-work-of-art-about-a-sinister-wooden-boy%2F&psig=AOvVaw1hrRugLceuyJLpGPLUPbuo&ust=1676314264024000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiiwrTTkP0CFQAAAAAdAAAAABAI", year: 2022}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <MovieItem/>
+      <MovieItem/>
+      <MovieItem/>
+    </React.Fragment>
   );
 }
 
--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,1 +1,27 @@
 import React from "react";
+import "../styles/MovieItem.css"
+
+function MovieItem(props){
+  return(
+    <li className="movieItem">
+      <article  className="shopCard">
+        <div className="imgContainer">
+            <img src={props.posterURL}/>
+        </div>
+        <div class="infoContainer">
+            <h2>{props.movieName}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam earum tempore eum facere dolor rerum suscipit corrupti consequuntur asperiores molestias nobis omnis.
+            </p>
+            <button className="addButton">
+                Add to favourites
+            </button>
+            <button className="deleteButton">
+                X
+            </button>
+        </div>
+        </article>
+    </li>
+  );
+}
+
+export {MovieItem};
